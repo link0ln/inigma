1433abad192a1de7dcbcd6075db7627e332c1096
link0ln <g.link0ln@gmail.com>
2025-06-07 15:54:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -264,4 +264,5 @@ async def health_check():
 
 if __name__ == "__main__":
     logger.info("Starting Inigma server")
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    port = os.getenv("PORT", "8000")
+    uvicorn.run(app, host="0.0.0.0", port=port, log_level="info")
